b279be14564ce18d078ccb68d0bd74a414dc1116
shunyanakamura <shunyanakamuraschool@gmail.com>
2023-11-14 14:29:33 +0900

    テストの追加と、それに関するエラー処理の修正

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, request, jsonify
 
 # フィボナッチ数列の関数
 def fibonacci(n):
+    if not isinstance(n, int):
+        raise ValueError("n must be an integer")
     if n <= 0:
         raise ValueError("n must be a positive integer")
     elif n == 1 or n == 2:
         return 1
-    else:
-        a, b = 1, 1
-        for i in range(2, n):
-            a, b = b, a + b
-        return b
+    a, b = 1, 1
+    for i in range(2, n):
+        a, b = b, a + b
+    return b
 
 # Flaskアプリケーション
 app = Flask(__name__)
@@ -18,7 +19,11 @@ app = Flask(__name__)
 @app.route('/fib', methods=['GET'])
 def get_fibonacci():
     try:
-        n = int(request.args.get('n'))
+        n_str = request.args.get('n')
+        if n_str is None or not n_str.isdigit():
+            raise ValueError("n must be a positive integer")
+
+        n = int(n_str)
         result = fibonacci(n)
         return jsonify({"result": result}), 200
     except ValueError as e:
